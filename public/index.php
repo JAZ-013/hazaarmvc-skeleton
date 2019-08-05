351b6ef8f6f0c13812546182c52a3aaf39166109
Jamie Carl <jamie@hazaarlabs.com>
2019-08-05 16:47:29 +1000

    Updated composer.json for new serve command

--- public/index.php
+++ public/index.php
@@ -1,5 +1,11 @@
 <?php
 
+// Check if the requested file is not this index.php file and the file actually exists
+if(php_sapi_name() === 'cli-server'
+    && $_SERVER['SCRIPT_FILENAME'] !== __FILE__
+    && is_file(realpath(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))))
+    return false;
+
 // Composer autoloading
 include __DIR__ . '/../vendor/autoload.php';
 
